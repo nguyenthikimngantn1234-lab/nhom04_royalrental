a1123a573d727d60ed16575182e4ec507ebba4ca
Kim Ngan <nguyenthikimngantn1234@gmail.com>
2026-04-26 23:34:36 +0700

    Deploy to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', 
+  // Điền tên repo vào đây, nhớ có dấu gạch chéo bao quanh
+  basePath: '/nhom04_royalrental', 
+  assetPrefix: '/nhom04_royalrental/', 
+  images: {
+    unoptimized: true, 
+  },
 };
 
 export default nextConfig;
